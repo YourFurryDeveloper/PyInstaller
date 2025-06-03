fe41d99991e3aad3d546f0f7093aa6446346b022
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-06-03 12:43:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ fileurls = []
 # And here, you add the folders that each file should go into. If they should stay in the current directory, just put "./". (The position of the item in this list corresponds to the position of the item in the list above.)
 folders = []
 
+# Here, you can specify any pip prerequisites or commands that need to run during the install process. If there are none, leave blank.
+cmds = []
+
 sudoRequired = False
 
 # ========================================================
@@ -32,12 +35,19 @@ def setLoadBar(loadprog):
 
 def installPrereqs():
     fileCount = 0
+    actionCount = 0
     setLoadBar(0)
+    if not len(cmds) == 0:
+        for cmd in cmds:
+            actionCount += 1
+            os.system(cmd)
+
     for file in fileurls:
+        actionCount += 1
         fileCount += 1
         os.system("clear")
         print(f"{BOLD}{GREEN}Installing {pkgName}{RESET}\n")
-        setLoadBar(int(100 / len(fileurls) * fileCount))
+        setLoadBar(int(100 / (len(fileurls) + len(cmds)) * actionCount))
         print(f"\nGetting {BOLD}{file}{RESET}...")
         os.makedirs(folders[fileCount - 1], exist_ok=True)
         if sudoRequired: os.system(f'sudo curl --progress-bar -o "{folders[fileCount - 1]}/$(basename {file})" "{file}"')
